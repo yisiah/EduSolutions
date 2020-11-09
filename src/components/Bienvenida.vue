0c7d7fe03f4f67368629ec29f2e2e01331854248
yisiah <harita.1818@gmail.com>
2020-11-09 04:28:48 -0600

    segundo commit

--- src/components/Bienvenida.vue
+++ src/components/Bienvenida.vue
@@ -25,38 +25,46 @@
 
 <template>
 
-<div class="container">
+<b-container fluid>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-
-
-                <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-                    <b-navbar-nav>
-                        <b-nav-item href="#">Home</b-nav-item>
-                        <b-nav-item href="#">About</b-nav-item>
-                        <b-nav-item href="#">Top</b-nav-item>
+        <div>
+            <b-navbar toggleable="lg" type="dark" variant="info">
+                <b-navbar-brand href="#">Logo</b-navbar-brand>
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-collapse id="nav-collapse" is-nav>
+                    <!-- Right aligned nav items -->
+                    <b-navbar-nav class="ml-auto">
+                        <b-navbar-nav>
+                            <b-nav-item href="#">Home</b-nav-item>
+                            <b-nav-item href="#">About</b-nav-item>
+                            <b-nav-item href="#">Top</b-nav-item>
+                        </b-navbar-nav>
+                        <b-nav-form>
+                            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                        </b-nav-form>
                     </b-navbar-nav>
-                    <b-nav-form>
-                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                    </b-nav-form>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
-    <div class="maincontainer">
-        <h1 class="titulo">Todo el conocimiento al alcance de tus manos.<br>
-        Inicie a usar cualquier herramienta colaborativa hoy</h1>
-        <b-button size="lg">Also Disabled</b-button>
+                </b-collapse>
+            </b-navbar>
+        </div>
+
+        <div class="maincontainer">
+            <b-container fluid>
+                <b-row>
+                    <h1 class="titulo">Todo el conocimiento al alcance de tus manos.<br>
+                        Inicie a usar cualquier herramienta colaborativa hoy.</h1>
+                </b-row>
+                <b-row>
+                    <b-button size="lg">Inicia</b-button>
+                </b-row>
+                <b-row>
+                    <b-button size="lg">Inicia</b-button>
+                </b-row>
+            </b-container>
+        </div>
 
     </div>
-</div>
+</b-container>
 
 </template>
 
