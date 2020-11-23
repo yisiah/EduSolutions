7fc7a2a7500cf5f18e3ac7144e9766949d166ad0
Daniel Rodriguez <danielrodp1@gmail.com>
2020-11-22 23:03:36 -0600

    Limpieza de CSS inutilizado, agregacion de imagenes/logos, ACTUALIZAR CON LINKS A PAGINAS FUTURAS

--- src/components/Bienvenida.vue
+++ src/components/Bienvenida.vue
@@ -1,26 +1,52 @@
 <style>
 
 .maincontainer {
-    /*border-radius: 25px;*/
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 20px;
-    box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.15);
-    width: 100%;
-    background-color: #31495E;
+  /*border-radius: 25px;*/
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.15);
+  width: 100%;
+  background-color: #31495E;
+  height:80vh;
+
 }
 
-.container {
-    margin-top: 40px;
+.logoHeader {
+  float: left;
+  width: 12%;
+  height: 12%;
+}
+
+.footer {
+  padding: 20px;
+  width: 100%;
+  background-color: #31495E;
+  padding-bottom: 3%;
 }
 
 .titulo {
-    color: white;
-    padding: 50px;
+  color: white;
+  margin: auto;
+  width: 55%;
+  font-weight: bold;
 }
 
+.button {
+  margin: auto;
+  margin-top: 2%;
+  margin-bottom: 2%;
+  padding: 10px;
+  background-color: #a876fe;
+}
+
+.splash {
+  margin: auto;
+  width: 85%;
+  height: 85%;
+}
 </style>
 
 <template>
@@ -29,25 +55,19 @@
     <div>
         <div>
             <b-navbar toggleable="lg" type="dark" variant="info">
-                <b-navbar-brand href="#">Logo</b-navbar-brand>
+                <b-navbar-brand href>  <img class="logoHeader" src="..\assets\LogoApp.png" alt="logo"> </b-navbar-brand>
                 <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
                 <b-collapse id="nav-collapse" is-nav>
                     <!-- Right aligned nav items -->
                     <b-navbar-nav class="ml-auto">
                         <b-navbar-nav>
-                            <b-nav-item href="#">Home</b-nav-item>
-                            <b-nav-item href="#">About</b-nav-item>
-                            <b-nav-item href="#">Top</b-nav-item>
+                            <b-nav-item href="#">Bienvenida</b-nav-item>
+                            <b-nav-item href="#">Sobre Nosotros</b-nav-item>
                         </b-navbar-nav>
-                        <b-nav-form>
-                            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                        </b-nav-form>
                     </b-navbar-nav>
                 </b-collapse>
             </b-navbar>
         </div>
-
         <div class="maincontainer">
             <b-container fluid>
                 <b-row>
@@ -55,17 +75,17 @@
                         Inicie a usar cualquier herramienta colaborativa hoy.</h1>
                 </b-row>
                 <b-row>
-                    <b-button size="lg">Inicia</b-button>
+                    <b-button class="button" size="lg" >Inicio</b-button>
                 </b-row>
                 <b-row>
-                    <b-button size="lg">Inicia</b-button>
+                  <img class="splash" src="..\assets\splash.png" alt="logo">
                 </b-row>
             </b-container>
         </div>
-
     </div>
+    <footer class="footer">
+    </footer>
 </b-container>
-
 </template>
 
 <script>
